b3adbcb8ccd3188872fc9ea8d37f27f2f8a05ea0
SHALIN <SHALIN@WINDOWS-21FI1FO>
2017-08-02 13:43:13 -0700

    Add project files.

---
+++ CarDB/default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="CarDB.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Car Mileage Web APP</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Car Mileage Web APP</h2>
+    <div>
+        
+        <asp:DataList ID="CarList" runat="server" DataKeyField="CarID" DataSourceID="SqlDataSource1" OnItemCommand="CarList_ItemCommand">
+            <ItemTemplate>
+                CarID:
+                <asp:Label ID="CarIDLabel" runat="server" Text='<%# Eval("CarID") %>' />
+                <br />
+                Car:
+                <asp:Label ID="CarLabel" runat="server" Text='<%# Eval("Car") %>' />
+                <br />
+                Manufact:
+                <asp:Label ID="ManufactLabel" runat="server" Text='<%# Eval("Manufact") %>' />
+                <br />
+                MPG:
+                <asp:Label ID="MPGLabel" runat="server" Text='<%# Eval("MPG") %>' />
+                <br />
+                <asp:LinkButton ID="LinkButton1" runat="server" CommandArgument="CarID" CommandName="Delete">Delete This Car</asp:LinkButton>
+                <br />
+            </ItemTemplate>
+            <SeparatorTemplate>
+                <hr />
+            </SeparatorTemplate>
+        </asp:DataList>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:MileageDBConnectionString %>" SelectCommand="SELECT * FROM [CarTable]"></asp:SqlDataSource>
+        <br />
+        
+    </div>
+
+        <hr />
+
+        <div>
+            <h4>Add a new car</h4>
+            <p>Car Make: <asp:TextBox ID="CarMake" runat="server"></asp:TextBox></p>
+            <p>Manafacure: <asp:TextBox ID="Manafacture" runat="server"></asp:TextBox></p>
+            <p>MPG: <asp:TextBox ID="MPG" runat="server" MaxLength="3"></asp:TextBox></p>
+            <asp:Button ID="ButtonAdd" runat="server" OnClick="ButtonAdd_Click" Text="Add" />
+            <br />
+        </div>
+            </form>
+    <h4>(C)Sunny WU 2017</h4>
+</body>
+</html>
---
+++ CarDB/default.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace CarDB
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void CarList_ItemCommand(object source, DataListCommandEventArgs e)
+        {
+            var theCarID = e.CommandArgument.ToString();
+            if (theCarID!="")
+            {
+                DeleteACar(theCarID);
+                Response.Redirect("default.aspx");
+            }
+            else
+            {
+                Response.Write("Nothing to delete!");
+            }
+
+        }
+
+        public void DeleteACar(string theCarID)
+        {
+            SqlConnection conn;
+            SqlCommand comm;
+            conn = new SqlConnection("Server=WINDOWS-21FI1FO\\DBA130;" + "Database=MileageDB;Integrated Security=True");
+            comm = new SqlCommand("DELETE FROM CarTable WHERE CarID = @theCarID limit 1", conn);
+
+            comm.Parameters.Add("@theCarID", System.Data.SqlDbType.VarChar);
+
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+            
+        }
+
+        protected void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn;
+            SqlCommand comm;
+            conn = new SqlConnection("Server=WINDOWS-21FI1FO\\DBA130;" + "Database=MileageDB;Integrated Security=True");
+
+            comm = new SqlCommand("INSERT INTO CarTable (Car, Manufact, MPG) VALUES (@CarMake, @Manafacture, @MPG)", conn);
+
+            comm.Parameters.Add("@CarMake", System.Data.SqlDbType.VarChar);
+            comm.Parameters["@CarMake"].Value = CarMake.Text;
+            comm.Parameters.Add("@Manafacture", System.Data.SqlDbType.VarChar);
+            comm.Parameters["@Manafacture"].Value = Manafacture.Text;
+            comm.Parameters.Add("@MPG", System.Data.SqlDbType.Int);
+            comm.Parameters["@MPG"].Value = MPG.Text;
+
+            conn.Open();
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            Response.Redirect("default.aspx");
+        }
+    }
+}
